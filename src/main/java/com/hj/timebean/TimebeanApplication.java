29f7f021c14c68eb1b95301c08710823c5096403
Hojeong016 <skflsehdn@naver.com>
2024-05-05 20:21:20 +0900

    Initial commit

---
+++ src/main/java/com/hj/timebean/TimebeanApplication.java
@@ -0,0 +1,13 @@
+package com.hj.timebean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimebeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimebeanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hj/timebean/TimebeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hj.timebean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimebeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
