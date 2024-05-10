54121f44a8984682df0c4edfd3040ad9c7911f20
Hojeong016 <skflsehdn@naver.com>
2024-05-10 23:02:11 +0900

    feat signInBlank

--- src/main/java/com/hj/timebean/controller/SignInRestController.java
+++ src/main/java/com/hj/timebean/controller/SignInRestController.java
@@ -9,15 +9,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
-@RequestMapping("signIn")
 public class SignInRestController {
     private final MemberService memberService;
     public SignInRestController(MemberService memberService) {
         this.memberService = memberService;
     }
-    @PostMapping
-    public ResponseEntity<String> signInBlank(@RequestBody SignInDTO signInDTO){
+    @PostMapping("/signInBlank")
+    public ResponseEntity<String> signInBlank(@Valid @RequestBody SignInDTO signInDTO){
         //공란 검증
         if (StringUtils.isBlank(signInDTO.getMemberId()) || StringUtils.isBlank(signInDTO.getPassword())) {
             return ResponseEntity.badRequest().body("아이디 또는 패스워드가 입력되지 않았습니다.");
