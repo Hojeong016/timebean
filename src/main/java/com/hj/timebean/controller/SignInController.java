8845236f981437ca8c181a0c7ea74fe50f1759d7
Hojeong016 <skflsehdn@naver.com>
2024-05-11 23:00:43 +0900

    feat signInBlank

--- src/main/java/com/hj/timebean/controller/SignInController.java
+++ src/main/java/com/hj/timebean/controller/SignInController.java
@@ -4,8 +4,8 @@ import com.hj.timebean.dto.SignInDTO;
 import com.hj.timebean.service.member.MemberService;
 import io.micrometer.common.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -28,23 +28,23 @@ public class SignInController {
     }
 
     @PostMapping
-    public String signIn(@Valid @ModelAttribute SignInDTO signInDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes){
+    public String signIn(@Valid @ModelAttribute SignInDTO signInDTO, RedirectAttributes redirectAttributes) {
 
-        if(bindingResult.hasErrors()){
-             return "signIn/signIn";}
-
-        if(StringUtils.isBlank(signInDTO.getMemberId()) || StringUtils.isBlank(signInDTO.getPassword())){
-            redirectAttributes.addFlashAttribute("error", "아이디 또는 비밀번호를 입력하세요.");
-            return "signIn/signIn";}
-
-        boolean result = memberService.login(signInDTO);
-
-        if (result == true) {
-            //랭킹창 반환
-            return"redirect:/";
-        } else {
-            //로그인 창 반환
-            return"signIn/signIn";
+        if (StringUtils.isBlank(signInDTO.getMemberId()) || StringUtils.isBlank(signInDTO.getPassword())) {
+            redirectAttributes.addFlashAttribute("error", "모든 정보를 입력해주세요.");
+            return "redirect:/signIn/signInView";
         }
-    }
+
+
+            boolean result = memberService.login(signInDTO);
+
+            if (result == true) {
+                //랭킹창 반환
+                return "redirect:/";
+            } else {
+                //로그인 창 반환
+                return "signIn/signIn";
+            }
+        }
+
 }
